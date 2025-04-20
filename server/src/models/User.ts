216b25d27a6957758549d2e3d2b995a40f29527c
Jesus24-Dev <siritjesus24@gmail.com>
2025-04-19 22:19:47 -0400

    feat: create User model with Sequelize for user data management

---
+++ server/src/models/User.ts
@@ -0,0 +1,43 @@
+import { DataTypes, Model } from 'sequelize';
+import sequelize from '../database/database';
+import UserAttributes from '../types/userType';
+
+class User extends Model<UserAttributes> implements UserAttributes {
+    public id!: number;
+    public username!: string;
+    public email!: string;
+    public password!: string;
+    public readonly createdAt!: Date;
+    public readonly updatedAt!: Date;
+  }
+  
+User.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'user',
+      timestamps: true,
+    }
+);
+  
+export default User;

22d1f32e033d74113e1a8a07bb07a8f8de511ba8
Jesus24-Dev <siritjesus24@gmail.com>
2025-04-19 22:19:32 -0400

    refactor: simplify server initialization by removing unnecessary code

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,22 +1,4 @@
-import express, {Request, Response} from 'express'
-import {Server} from 'socket.io'
-import http from 'http'
-import dotenv from 'dotenv'
-import cors from 'cors'
-
-dotenv.config()
-
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
-
-const server = http.createServer(app);
-
-app.get('/api/hello', (req: Request, res: Response) => {
-    res.status(200).json({message: "hello world from backend"})
-})
-
+import server from "./app"
 const PORT = process.env.PORT || 3030
 
 server.listen(PORT, () => {

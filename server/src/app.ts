81115385010fc862733fb1d2a29b22a28397647f
Jesus24-Dev <siritjesus24@gmail.com>
2025-04-21 19:19:41 -0400

    feat: set up socket.io server with CORS configuration and authentication

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,6 +5,8 @@ import dotenv from 'dotenv'
 import cors from 'cors'
 import authRouter from './routes/auth.routes'
 import userRouter from './routes/user.routes'
+import { socketAuth } from './sockets/socketAuth'
+import { socketConnect } from './sockets/socketConnect'
 
 dotenv.config()
 const app = express()
@@ -13,6 +15,17 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
 const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: process.env.FRONTEND_URL,
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+})
+
+socketAuth(io)
+socketConnect(io);
 
 app.get('/api/hello', (req: Request, res: Response) => {
     res.status(200).json({message: "hello world from backend"})

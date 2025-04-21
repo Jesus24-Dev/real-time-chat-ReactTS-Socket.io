75a25e1dde27ad40fb93afa6e84e500769c19328
Jesus24-Dev <siritjesus24@gmail.com>
2025-04-21 14:45:06 -0400

    refactor: reorganize routes and remove database connection logic

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,11 +3,10 @@ import {Server} from 'socket.io'
 import http from 'http'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import authRouter from './routes/auth.routes'
 import userRouter from './routes/user.routes'
-import sequelize from './database/database'
 
 dotenv.config()
-
 const app = express()
 app.use(cors())
 app.use(express.json())
@@ -15,22 +14,11 @@ app.use(express.urlencoded({ extended: true }));
 
 const server = http.createServer(app);
 
-const database = async () => {
-    try {
-        await sequelize.authenticate();
-        await sequelize.sync();
-        console.log('Conexión a la base de datos establecida correctamente.');
-      } catch (error) {
-        console.error('Error al conectar con la base de datos:', error);
-      }
-}
-
-database();
-
 app.get('/api/hello', (req: Request, res: Response) => {
     res.status(200).json({message: "hello world from backend"})
 })
 
-app.use('/users', userRouter)
+app.use('/auth', authRouter)
+app.use('/user', userRouter)
 
 export default server;

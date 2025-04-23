74357e67eb5b271bd88668cf0c5dffe92fd950a8
Jesus24-Dev <siritjesus24@gmail.com>
2025-04-23 11:03:38 -0400

    feat: add FormField component for reusable input fields

--- client/src/components/ui/FormField.tsx
+++ client/src/components/ui/FormField.tsx
@@ -3,7 +3,7 @@ interface FormFieldProps {
     type: string;
     name: string;
     value?: string;
-    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 export default function FormField({label, type, name, value, onChange}: FormFieldProps){

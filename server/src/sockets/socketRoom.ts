5f2a811c21099f48bc7a66fe6a5fcf416eeaa74d
Jesus24-Dev <siritjesus24@gmail.com>
2025-04-23 12:57:29 -0400

    feat: update socketRoom to use string for roomId in event handlers

--- server/src/sockets/socketRoom.ts
+++ server/src/sockets/socketRoom.ts
@@ -1,17 +1,21 @@
-export function socketRoom(socket: any, io: any){
-    socket.on('join_room', (roomId: number, userId: number) => {
-        socket.join(roomId.toString())
-    })
+import MessageAttributes from "../types/messageType"
 
-    socket.on('leave_room', (roomId: number, userId: number) => {
-        socket.leave(roomId.toString())
-    })
+export function socketRoom(socket: any, io: any) {
+    socket.on('join_room', (roomId: string, userId?: number) => { // Cambiado a string
+        console.log(`Usuario ${userId} uniéndose a sala ${roomId}`);
+        socket.join(roomId);
+    });
+
+    socket.on('leave_room', (roomId: string, userId: number) => { // Cambiado a string
+        socket.leave(roomId);
+    });
 
     socket.on('created_room', () => {
         io.emit('room_created')
     })
 
-    socket.on('send_message', (roomId: number, message: string) => {
-        socket.to(roomId.toString()).emit('receive_message', message)
-    })
+    socket.on('send_message', (roomId: string, message: MessageAttributes) => { // Cambiado a string
+        console.log(`Mensaje recibido en sala ${roomId}:`, message);
+        io.to(roomId).emit('receive_message', message);
+    });
 }

9a285689a61717bffa8b715e9405c31df7a06f73
Jesus24-Dev <siritjesus24@gmail.com>
2025-04-30 17:25:33 -0400

    fix: refactor joinRoom function to use updateRoomId for room management

--- client/src/components/room/UserRoomList.tsx
+++ client/src/components/room/UserRoomList.tsx
@@ -3,11 +3,13 @@ import RoomData from "../../types/roomDataType";
 import useSocket from '../../hooks/useSocket';
 import Button from '../ui/Button';
 import Room from '../ui/Room';
+import { useRoom } from '../../hooks/useRoom';
 
 export default function UserRoomList(){
     const [room, setRoom] = useState<RoomData[]>([])
     const [userId, setUserId] = useState<number | null>(null)
     const {socket} = useSocket()
+    const {updateRoomId} = useRoom();
 
     useEffect(() => {
         const user = localStorage.getItem('user');
@@ -46,9 +48,8 @@ export default function UserRoomList(){
     const joinRoom = (roomId: number | undefined) => {
         if (roomId){
             const roomIdStr = roomId.toString();
-            localStorage.setItem('roomId', roomIdStr);
-            socket?.emit('join_room', roomIdStr, userId);
-            console.log(`Uniéndose a sala: ${roomIdStr}`);
+            updateRoomId(roomIdStr);
+            socket?.emit('join_room', roomIdStr, userId);    
         } 
     }
 

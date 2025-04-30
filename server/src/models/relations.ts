6bafb4521577e1376e9cb9e47f832cd268194fae
Jesus24-Dev <siritjesus24@gmail.com>
2025-04-30 17:26:54 -0400

    feat: establish relationships between User and Contact models for contact management

--- server/src/models/relations.ts
+++ server/src/models/relations.ts
@@ -1,7 +1,11 @@
 import User from "./User";
 import Room from './Room'
+import Contact from "./Contact";
 
 Room.belongsToMany(User, { through: 'RoomUser' });
 User.belongsToMany(Room, { through: 'RoomUser' });
 
-export {Room, User}
+User.belongsToMany(User, { through: Contact, foreignKey: 'id_user', as: 'contacts' });
+Contact.belongsTo(User, { foreignKey: 'id_user', as: 'user' });
+
+export {Room, User, Contact}

3c50e31966059433e17f397abb36608bca6b03cd
Jesus24-Dev <siritjesus24@gmail.com>
2025-05-01 13:06:57 -0400

    feat: add status property to UserAttributes interface for user state management

--- server/src/types/userType.ts
+++ server/src/types/userType.ts
@@ -2,6 +2,7 @@ export default interface UserAttributes {
     id?: string;
     username: string;
     email: string;
+    status?: 'offline' | 'online',
     password: string;
     createdAt?: Date;
     updatedAt?: Date;

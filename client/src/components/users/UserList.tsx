3767e67780cf7bf4715aa5465453326ddc1cfdfa
Jesus24-Dev <siritjesus24@gmail.com>
2025-05-01 15:43:02 -0400

    feat: implement add contact functionality in UserList component

--- client/src/components/users/UserList.tsx
+++ client/src/components/users/UserList.tsx
@@ -1,12 +1,15 @@
 import {useState, useEffect} from 'react';
 import UserCardProps from "../../types/userType";
 import UserCard from "../ui/UserCard";
+import Button from '../ui/Button';
+import useSocket from '../../hooks/useSocket';
 
 const UserList = () => {
 
     const [users, setUsers] = useState<UserCardProps[]>([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<string | null>(null)
+    const {socket} = useSocket()
 
     useEffect(() => {
         const fetchUsers = async () => {
@@ -40,9 +43,45 @@ const UserList = () => {
             }
         };
 
-        fetchUsers();
-    }, [])
+        if(socket){
+            socket?.on('update_contact', fetchUsers)
 
+            return () => {
+                socket?.off('update_contact', fetchUsers)
+            }
+        }
+        
+        fetchUsers();
+    }, [socket])
+
+    const addContact = async (id: string | undefined) => {
+        const user = localStorage.getItem('user')
+        const parsedUser = user ? JSON.parse(user) : null
+        if(!parsedUser || !id) {
+            return
+        }
+            try {
+                const response = await fetch(`http://localhost:3030/api/contact`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${localStorage.getItem('token')}`
+                    },
+                    body: JSON.stringify({
+                        id_user: parsedUser.id,
+                        id_contact: id
+                    })
+                });
+                if (!response.ok) {
+                    throw new Error('Failed to fetch users');
+                }
+                socket?.emit('add_contact')
+            } catch (error) {
+                const errorMessage = error instanceof Error ? error.message : 'An unknown error occurred';
+                console.log(errorMessage);
+            } 
+
+    }
 
     return (
         <div className="bg-amber-50 rounded-xl p-5 shadow-inner">
@@ -52,7 +91,9 @@ const UserList = () => {
             {users.length > 0 ? (
                 <div className="space-y-4">
                     {users.map((user) => (                      
-                            <UserCard key={user.id} username={user.username} status={user.status} id={user.id}/>
+                            <UserCard key={user.id} username={user.username} status={user.status} id={user.id}>
+                                <Button label="Add" type="button" onClick={() => addContact(user.id ? user.id : undefined)}/>
+                            </UserCard>
                     ))}
                 </div>
             ) : (

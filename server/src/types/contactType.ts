d837a802a803ee5f4e898b2570ac9652ad09baf7
Jesus24-Dev <siritjesus24@gmail.com>
2025-05-01 13:06:53 -0400

    feat: add contactUser property to ContactAttributes for enhanced user information

--- server/src/types/contactType.ts
+++ server/src/types/contactType.ts
@@ -2,6 +2,10 @@ export default interface ContactAttributes {
     id?: string;
     id_user: string;
     id_contact: string;
+    contactUser?: {
+        username: string,
+        status: string,
+    }
     createdAt?: Date;
     updatedAt?: Date;
 }

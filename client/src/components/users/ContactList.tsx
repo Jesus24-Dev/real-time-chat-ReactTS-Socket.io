259447acf117ef7c4b632c3946df0f148bf81063
Jesus24-Dev <siritjesus24@gmail.com>
2025-05-01 13:40:39 -0400

    fix: remove console log from fetchContacts function in ContactList component

--- client/src/components/users/ContactList.tsx
+++ client/src/components/users/ContactList.tsx
@@ -27,7 +27,6 @@ export default function ContactList() {
             throw new Error('Failed to fetch users');
         }
            const data = await response.json();
-          console.log(data)
            if(data.error) {
             setError(data.error)
             return
